4ae4c050f1ffe742b07cc81e8a78253513a57f54
Keisuke Tada <keisuke-tada@cookpad.com>
2014-10-19 00:14:57 +0900

    Add shell script for homebrew instead of brewfile

---
+++ homebrew.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew update
+
+brew install git
+brew install zsh
+brew install emacs
+brew install tmux
+brew install reattach-to-user-namespace
+brew install terminal-notifier
+brew install tig
+brew install hub
+brew install wget
+brew install the_silver_searcher
+brew install jq
+brew install aspell --lang=en
+brew install autossh
+
+brew tap peco/peco
+brew install peco
+
+brew cleanup
