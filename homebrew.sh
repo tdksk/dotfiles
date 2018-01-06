f0e27ae5f173c708884ccebefe978af057a72d22
Keisuke Tada <keisuke-tada@cookpad.com>
2018-01-06 14:56:40 +0900

    Update installing brew packages

--- homebrew.sh
+++ homebrew.sh
@@ -5,20 +5,18 @@ ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/
 brew update
 
 brew install git
+brew install go
 brew install zsh
 brew install emacs
+brew install vim --with-override-system-vi
 brew install tmux
 brew install reattach-to-user-namespace
 brew install terminal-notifier
 brew install tig
 brew install hub
-brew install wget
-brew install the_silver_searcher
 brew install jq
 brew install aspell --lang=en
 brew install cmigemo
-brew install autossh
-brew install chisel
 brew install peco
 
 brew cleanup
